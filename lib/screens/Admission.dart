fedbf43d6fc18877cc47bed85c38aaff1e571760
yunweneric <yunweneric@gmail.com>
2020-10-03 13:49:58 +0100

    removed debug banner

---
+++ lib/screens/Admission.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Admission extends StatefulWidget {
+  @override
+  _AdmissionState createState() => _AdmissionState();
+}
+
+class _AdmissionState extends State<Admission> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
